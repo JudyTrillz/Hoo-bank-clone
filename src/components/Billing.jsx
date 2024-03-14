e562a1b55accc23fb9c36aca05567d5993decd64
Judy Odiakose <ojudy007@gmail.com>
2024-03-14 15:23:35 +0100

    Finished the Billing Section

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,8 +1,48 @@
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
+
 const Billing = () => {
   return (
-    <div>
-      <h1>Billing</h1>
-    </div>
+    <section id='product' className={`${layout.sectionReverse}`}>
+      <div className={`${layout.sectionImgReverse}`}>
+        <img
+          src={bill}
+          alt='Billing Banner'
+          className='w-[100%] h-[100%] relative z-5'
+        />
+
+        <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-fill white__gradient' />
+        <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-fill pink__gradient' />
+      </div>
+
+      <div className={`${layout.sectionInfo}`}>
+        <h2 className={`${styles.heading2}`}>
+          Easily control your <br className='hidden sm:block' /> billing &
+          invoicing{" "}
+        </h2>
+
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium,
+          quos. Vero, quis. Sint, omnis reprehenderit? Adipisci consequuntur
+          nisi perferendis iusto optio molestias aliquid corrupti? Ipsum et
+          deserunt unde laboriosam reiciendis?
+        </p>
+
+        <div className='flex flex-row flex-wrap mt-6 sm:mt-10'>
+          <img
+            src={apple}
+            alt='Apple Store'
+            className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer'
+          />
+
+          <img
+            src={google}
+            alt='Google play'
+            className='w-[128px] h-[42px] object-contain  cursor-pointer'
+          />
+        </div>
+      </div>
+    </section>
   );
 };
 
