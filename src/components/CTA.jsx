8b6264b444cd491587d6a0ccd2276ca13894669a
Judy Odiakose <ojudy007@gmail.com>
2024-03-15 02:42:58 +0100

    Finished commit for the CTA section

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,8 +1,24 @@
+import styles from "../style";
+import Button from "./Button";
+
 const CTA = () => {
   return (
-    <div>
-      <h1>CTA</h1>
-    </div>
+    <section
+      className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} flex-col md:flex-row bg-black-gradient-2 rounded-[20px] box-shadow`}>
+      <div className='flex-1 flex flex-col'>
+        <h2 className={`${styles.heading2}`}>Try our services now!</h2>
+
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Everything you need to accept card payments{" "}
+          <br className='hidden sm:block' /> and grow your business anywhere on
+          the planet.
+        </p>
+      </div>
+
+      <div className={`${styles.flexCenter} ml-0 sm:ml-10 mt-10 md:mt-0`}>
+        <Button />
+      </div>
+    </section>
   );
 };
 
